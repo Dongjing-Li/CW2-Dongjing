465eb2024c360e75368bf738430ce2cd28476471
DongjingLi <69388254+DongjingLi@users.noreply.github.com>
2021-12-22 15:17:43 +0000

    Coursework complete

--- user.py
+++ user.py
@@ -3,7 +3,8 @@ import hashlib
 
 class User(object):
 
-    def __init__(self, first_name, last_name, email_address, username=""):
+    def __init__(self, password, first_name, last_name, email_address, username=""):
+        self.password = password
         self.first_name = first_name
         self.last_name = last_name
         self.email_address = email_address
